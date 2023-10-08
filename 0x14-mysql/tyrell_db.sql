122bef567c597a920eb4d48b28cc561bfbb8e344
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2023-10-08 06:31:18 -0700

    created source replica connection

---
+++ 0x14-mysql/tyrell_db.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE tyrell_corp;
+USE tyrell_corp;
+CREATE TABLE nexus6 (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+INSERT INTO nexus6 (name) VALUES ('Leon');
