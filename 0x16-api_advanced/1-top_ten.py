875ea2ea077ce913ff8e6d1fa57be93c7408f182
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2023-10-13 16:09:23 -0700

    1-top_ten.py

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" queries the Reddit API and prints the titles of the first 10 hot\
+posts listed for a given subreddit.  """
+
+import requests
+
+
+def top_ten(subreddit):
+    """Queries the Reddit API to retrieve subreddit title details."""
+    headers = {
+        "User-Agent": "ubuntu:alx.APIadavncedProject:v1.0 \
+(by /u/don-wiseman-1903)"
+    }
+    api_url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    val = {
+        "limit": 15,
+        "count": 0
+    }
+    try:
+        r = requests.get(api_url, params=val, headers=headers,
+                         allow_redirects=False)
+        r.raise_for_status()
+        r_j = r.json()
+        if r_j:
+            children = r_j.get('data').get('children')
+            for i in range(0, 10):
+                print(children[i].get('data').get('title'))
+    except requests.exceptions.RequestException as e:
+        return None
+    except ValueError as e:
+        return None
+
+
+if __name__ == '__main__':
+    top_ten('programming')
