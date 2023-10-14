430225ca9b7a1e0d0db40e32e3f01c6bf0427993
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2023-10-14 07:56:21 -0700

    0-strace_is_your_friend.pp longlines correction

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 # This is a puppet command to correct an error in the wp settings.
 exec { 'edit_wp-settings.php':
-  command => 'sed -i "s/require_once( ABSPATH . WPINC . \'\/class-wp-locale.phpp\' );/require_once( ABSPATH . WPINC . \'\/class-wp-locale.php\' );/" /var/www/html/wp-settings.php',
+  command => 'sed -i "s/require_once( ABSPATH . WPINC . \'\/class-wp-locale.phpp\' );/require_once( ABSPATH . WPINC . \'\/class-wp-\
+locale.php\' );/" /var/www/html/wp-settings.php',
   path    => '/bin:/usr/bin', # Set the path to ensure the 'sed' command is found
   onlyif  => 'test -f /var/www/html/wp-settings.php', # Check if the file exists before running the command
 }
